64f6983892f6c6f77a9396ca974d5a8f87b19960
y.yakubovich <y.yakubovich@softteco.com>
2024-04-02 14:47:20 +0300

    backend app with json server

---
+++ index.js
@@ -0,0 +1,48 @@
+import { readFileSync } from 'fs';
+import { create, router as _router, defaults, bodyParser } from 'json-server';
+import { resolve } from 'path';
+
+const server = create();
+
+const router = _router(resolve(__dirname, 'db.json'));
+
+server.use(defaults({}));
+server.use(bodyParser);
+
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(readFileSync(resolve(__dirname, 'db.json'), 'UTF-8'));
+        const { users = [] } = db;
+
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password,
+        );
+
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+// проверяем, авторизован ли пользователь
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
+
+    next();
+});
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
